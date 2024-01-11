46e0d48c4869ece1ecad580e35df04233dfc1b43
acubwe <artemchibarchicov@gmail.com>
2024-01-12 01:43:10 +0300

    добавление гидов

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+'use strict'
 const apiKey = "7a70d143-15d9-4666-8aab-21c06fcfd6f9"
 const mainUrl = "http://exam-2023-1-api.std-900.ist.mospolytech.ru"
 const routeUrl = "/api/routes"
 
 function selectRoute(event){
-
+    alert("sdkfl;")
 }
 
+
+const tbody = document.querySelector(".tbody");
 function addTableData(record) {
     const tr = document.createElement('tr');
     tr.id = record.id;
@@ -13,40 +16,89 @@ function addTableData(record) {
     name.textContent = record.name;
     tr.append(name);
     const description = document.createElement('td');
-    description.textContent = record.descriprion;
+    description.textContent = record.description;
     tr.append(description);
     const mainObject = document.createElement('td');
     mainObject.textContent = record.mainObject;
     tr.append(mainObject);
     const tdBtn = document.createElement("td")
     tdBtn.textContent = "Выбрать";
-    tdBtn.addEventListener("click", selectRoute);
+    tdBtn.addEventListener("click", event => guidsData(tr, event));
+    tr.append(tdBtn);
+    tbody.appendChild(tr);
+
 }
 
-function splitObject(mainObject) {
-    if (mainObject.match(/, /g)>=mainObject.match(/\./g) && mainObject.match(/,/g) > mainObject.match(/-/g)) {
-        return mainObject.split("-")
+function splitMainObject(value) {
+    console.log(value.match(/,/g)?.length)
+    if (value.match(/,/g)?.length>=value.match(/\./g)?.length && value.match(/,/g)?.length>value.match(/-/g)?.length) {
+        return value.split(',');
     }
+    if (value.match(/\./g)?.length>value.match(/-/g)?.length && value.match(/\./g)?.length>=value.match(/,/g)?.length) {
+        return value.split('.');
+    }
+        return value.split('-')
 }
+
 
 function getData() {
     const xhr = new XMLHttpRequest();
     const url = new URL(routeUrl, mainUrl);
     url.searchParams.set('api_key', apiKey);
-    xhr.open('get', url);
-    xhr.send();
+    xhr.open('GET', url);
     xhr.onload = function() {
         const records = JSON.parse(xhr.response);
-        const tbody = document.querySelector('.tbody');
         for (const record of records) {
-            console.log(record);
-            tbody.innerHTML += `<tr id = ${record.id}>
-            <td>${record.name}</td>
-            <td>${record.description}</td>
-            <td>${record.mainObject}</td>
-            <td><button>Выбрать</button></td>`
+            const select = document.querySelector('.form-select');
+            for (const elem of splitMainObject(record.mainObject)) {
+                const option = document.createElement("option");
+                option.textContent = elem;
+                select.append(option);
+            };
+            addTableData(record);
         }
     }
+    xhr.send();
+}
+
+function guidsData(tr, event) {
+    const guidsRoad = tr.id;
+    const guidesUrl = `/api/routes/${guidsRoad}/guides`;
+    const xhr = new XMLHttpRequest();
+    const newUrl = new URL(guidesUrl, mainUrl);
+    newUrl.searchParams.set('api_key', apiKey);
+    xhr.open("GET", newUrl);
+    xhr.onload = function() {
+        const records = JSON.parse(xhr.response);
+        console.log(guidesUrl);
+        for (const record of records) {
+            console.log(record);
+            addDataGuids(record);
+        }
+    };
+    xhr.send();
+}
+
+const tbodyguids = document.querySelector(".tbody-guids")
+
+
+function addDataGuids(record) {
+    const tr = document.createElement('tr');
+    tr.id = record.id;
+    const name = document.createElement('td');
+    name.textContent = record.name;
+    tr.append(name);
+    const language = document.createElement('td');
+    language.textContent = record.language;
+    tr.append(language);
+    const workExperience = document.createElement('td');
+    workExperience.textContent = record.workExperience;
+    tr.append(workExperience);
+    const pricePerHour = document.createElement('td');
+    pricePerHour.textContent = record.pricePerHour;
+    tr.append(pricePerHour);
+    const tdBtn = document.createElement("td")
+    tbodyguids.appendChild(tr)
 }
 
 window.addEventListener("DOMContentLoaded", ()=>{
